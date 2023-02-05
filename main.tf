f1029c113f4bc90b0ca96cc2b94548be86f7a1d6
Chedy Missaoui <missaoui.chedy@gmail.com>
2023-02-05 15:10:54 +0100

    renamed resources + plugged resources on created Resource Group

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "azurerm_resource_group" "rg" {
   name     = random_pet.rg_name.id
 }
 
-resource "azurerm_network_security_rule" "res-5" {
+resource "azurerm_network_security_rule" "allow-ssh-nsg-rule" {
   access                      = "Allow"
   destination_address_prefix  = "*"
   destination_port_range      = "22"
@@ -16,24 +16,21 @@ resource "azurerm_network_security_rule" "res-5" {
   network_security_group_name = "KABLAM001-nsg"
   priority                    = 300
   protocol                    = "Tcp"
-  resource_group_name         = "rg-chedy-test"
+  resource_group_name         = azurerm_resource_group.rg.name
   source_address_prefix       = "*"
   source_port_range           = "*"
   depends_on = [
-    azurerm_network_security_group.res-4,
+    azurerm_network_security_group.main-nsg,
   ]
 }
-resource "azurerm_resource_group" "res-0" {
-  location = "westeurope"
-  name     = "rg-chedy-test"
-}
-resource "azurerm_subnet" "res-8" {
+
+resource "azurerm_subnet" "main-subnet" {
   address_prefixes     = ["10.0.0.0/24"]
   name                 = "default"
-  resource_group_name  = "rg-chedy-test"
+  resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = "rg-chedy-test-vnet"
   depends_on = [
-    azurerm_virtual_network.res-7,
+    azurerm_virtual_network.main-vnet,
   ]
 }
 resource "azurerm_linux_virtual_machine" "res-1" {
@@ -43,7 +40,7 @@ resource "azurerm_linux_virtual_machine" "res-1" {
   location                        = "westeurope"
   name                            = "KABLAM001"
   network_interface_ids           = ["/subscriptions/07093206-6009-4977-9d2c-7b88cda14e92/resourceGroups/rg-chedy-test/providers/Microsoft.Network/networkInterfaces/kablam00161"]
-  resource_group_name             = "rg-chedy-test"
+  resource_group_name             = azurerm_resource_group.rg.name
   size                            = "Standard_DS1_v2"
   os_disk {
     caching              = "ReadWrite"
@@ -56,31 +53,31 @@ resource "azurerm_linux_virtual_machine" "res-1" {
     version   = "latest"
   }
   depends_on = [
-    azurerm_network_interface.res-2,
+    azurerm_network_interface.main-nic,
   ]
 }
-resource "azurerm_virtual_network" "res-7" {
+resource "azurerm_virtual_network" "main-vnet" {
   address_space       = ["10.0.0.0/16"]
   location            = "westeurope"
   name                = "rg-chedy-test-vnet"
-  resource_group_name = "rg-chedy-test"
+  resource_group_name = azurerm_resource_group.rg.name
   depends_on = [
-    azurerm_resource_group.res-0,
+    azurerm_resource_group.rg,
   ]
 }
-resource "azurerm_network_interface_security_group_association" "res-3" {
+resource "azurerm_network_interface_security_group_association" "main-nsg-nic-association" {
   network_interface_id      = "/subscriptions/07093206-6009-4977-9d2c-7b88cda14e92/resourceGroups/rg-chedy-test/providers/Microsoft.Network/networkInterfaces/kablam00161"
   network_security_group_id = "/subscriptions/07093206-6009-4977-9d2c-7b88cda14e92/resourceGroups/rg-chedy-test/providers/Microsoft.Network/networkSecurityGroups/KABLAM001-nsg"
   depends_on = [
-    azurerm_network_interface.res-2,
-    azurerm_network_security_group.res-4,
+    azurerm_network_interface.main-nic,
+    azurerm_network_security_group.main-nsg,
   ]
 }
-resource "azurerm_network_interface" "res-2" {
+resource "azurerm_network_interface" "main-nic" {
   enable_accelerated_networking = true
   location                      = "westeurope"
   name                          = "kablam00161"
-  resource_group_name           = "rg-chedy-test"
+  resource_group_name           = azurerm_resource_group.rg.name
   ip_configuration {
     name                          = "ipconfig1"
     private_ip_address_allocation = "Dynamic"
@@ -88,25 +85,25 @@ resource "azurerm_network_interface" "res-2" {
     subnet_id                     = "/subscriptions/07093206-6009-4977-9d2c-7b88cda14e92/resourceGroups/rg-chedy-test/providers/Microsoft.Network/virtualNetworks/rg-chedy-test-vnet/subnets/default"
   }
   depends_on = [
-    azurerm_public_ip.res-6,
-    azurerm_subnet.res-8,
+    azurerm_public_ip.main-publicip,
+    azurerm_subnet.main-subnet,
   ]
 }
-resource "azurerm_network_security_group" "res-4" {
+resource "azurerm_network_security_group" "main-nsg" {
   location            = "westeurope"
   name                = "KABLAM001-nsg"
-  resource_group_name = "rg-chedy-test"
+  resource_group_name = azurerm_resource_group.rg.name
   depends_on = [
-    azurerm_resource_group.res-0,
+    azurerm_resource_group.rg,
   ]
 }
-resource "azurerm_public_ip" "res-6" {
+resource "azurerm_public_ip" "main-publicip" {
   allocation_method   = "Static"
   location            = "westeurope"
   name                = "KABLAM001-ip"
-  resource_group_name = "rg-chedy-test"
+  resource_group_name = azurerm_resource_group.rg.name
   sku                 = "Standard"
   depends_on = [
-    azurerm_resource_group.res-0,
+    azurerm_resource_group.rg,
   ]
 }
